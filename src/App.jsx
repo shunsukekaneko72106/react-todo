0c8c88e0eea364e51658fbf3ed00396d44a3d9da
shunsuke kaneko <61620829+shunsukekaneko72106@users.noreply.github.com>
2021-07-18 16:56:17 +0900

    add code

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  //input
+  const [todoText, setTodoText] = useState("");
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  //未完了のtodo
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    "テストテスト",
+    "hogehoge"
+  ]);
+  //完了のtodo
+  const [completeTodos, setCompleteTodos] = useState(["uuuu", "eeee"]);
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
         <button>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          <div className="list-row">
-            <li>テストテスト</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
           <div className="list-row">
             <li>hogehoge</li>
             <button>完了</button>
@@ -26,10 +46,15 @@ export const App = () => {
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
-          <div className="list-row">
-            <li>テストテスト</li>
-            <button>戻す</button>
-          </div>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
+
           <div className="list-row">
             <li>hogehoge</li>
             <button>戻す</button>
